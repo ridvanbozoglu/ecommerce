e710c1b89519e1ccf4a394f486a1f360a0eb482b
ridvanbozoglu <ridvanbozoglu.5@gmail.com>
2024-09-02 16:55:44 +0300

    categorySlice is done.

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import clientReducer from './reducers/clientReducer';
 import productReducer from './reducers/productReducer';
 import shoppingCartReducer from './reducers/shoppingCartReducer';
+import categoriesSlice from './reducers/catagoriesReducer';
 import logger from 'redux-logger';
 
 const store = configureStore({
@@ -9,6 +10,7 @@ const store = configureStore({
     client: clientReducer,
     product: productReducer,
     shoppingCart: shoppingCartReducer,
+    categories:categoriesSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(logger),
